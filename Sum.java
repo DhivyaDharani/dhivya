e626c3c330d13dd289211cee71ef8f97c5c3ff30
DhivyaDharani <divyadd2117@gmail.com>
2017-07-01 10:48:08 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -9,10 +9,16 @@ int i=0,a;
 int sum=0;
 System.out.println("enter the value of last number");
 a=sc.nextInt();
+  if(a>0)
+  {
 for(i=0;i<a;i++)
 {
 sum=sum+i;
 }
 System.out.println("The sum is"+sum);
+    else
+    {
+      System.out.println("The given number is not a natural number");
+    }
 }
 }
