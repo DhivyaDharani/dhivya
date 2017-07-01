2c5613889c245011c555a9b848e70d173fcd4870
DhivyaDharani <divyadd2117@gmail.com>
2017-07-01 10:26:19 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class LeapYear
 {
-public static void main(string args[])
+public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a;
