fad0fcb9a03cf0fbb4b82a5b1bc715b6e1d32162
Dhivya Dharani <divyadd2117@gmail.com>
2017-07-06 14:35:18 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n,sum=0;
-System.out.println("enter the value you want to check");
+System.out.println("Enter the value you want to check");
 n=sc.nextInt();
 int v==n;
 while(n>0)
