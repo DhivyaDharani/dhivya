55f88a2d1a7f778f3da637793ae81b3fa4fb1a29
Dhivya Dharani <divyadd2117@gmail.com>
2017-07-06 14:41:55 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Prime
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n,i=;
+System.out.println("enter the number you want to check");
+n=sc.nextInt();
+if(n%1==n%n)
