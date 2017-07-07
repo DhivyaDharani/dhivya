34573ff13ce105a02a06b338370f1cd92e96c441
Dhivya Dharani <divyadd2117@gmail.com>
2017-07-07 12:27:56 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Conversion
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new InputstreamReader(System.in));
+String numberAsString = "1234";
+int number = Integer.parseInt(numberAsString);
+System.out.println("The number is: " + number);
+}
+}
