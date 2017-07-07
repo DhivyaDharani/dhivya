5593c02fbbc724174b0ee1c01bf23ae282c2d5c3
Dhivya Dharani <divyadd2117@gmail.com>
2017-07-07 09:40:23 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -5,7 +5,24 @@ public class Prime
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-int n,i=;
+int i,n,m=0,flag=0;
 System.out.println("enter the number you want to check");
 n=sc.nextInt();
-if(n%1==n%n)
+
+m=n/2;
+  for(i=2;i<=m;i++)
+  {
+    if(n%i==0)
+    {
+    System.out.println("The given number is not a prime");
+  flag==1;
+    break;
+  }
+}
+  if(flag==0)
+  {
+    System.out.println("The given number is prime");
+  }
+}
+}
+    
