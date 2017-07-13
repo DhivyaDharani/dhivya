ba915a5b1fee0401c885436f731f9866ed328b2c
Dhivya Dharani <divyadd2117@gmail.com>
2017-07-13 11:23:29 +0530

    Update Camel.java

--- Camel.java
+++ Camel.java
@@ -1,24 +1,29 @@
+
+import java.io.IOException;
 import java.util.*;
 public class Camel
 {
-public static void main(String args[])
+public static void main(String args[])throws IOException
+
 {
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter a sentence");
 String a=sc.nextLine();
-String res="";
-char first = input.charAt(0);
-res = res + Character.toUpperCase(firstChar);
-for(int i=0;i<a.length();i++)
+String result= "";
+
+char first =a.charAt(0);
+
+result = result + Character.toUpperCase(first);
+for(int i=1;i<a.length();i++)
 {
-char spot1 = a.charAt(i);
-            char previousChar = a.charAt(i - 1);
-            if (previousChar == ' ') {
-                res = res + Character.toUpperCase(spot1);
+char spot1= a.charAt(i);
+            char preChar = a.charAt(i - 1);
+            if (preChar == ' ') {
+                result = result + Character.toUpperCase(spot1);
             } else {
-                res = res + spot1;
+                result = result + spot1;
             }
         }
-        System.out.println(res);
+        System.out.println(result);
     }
 }
